598dc509e9e2576f3bebd4bcc1857a741a412993
nilisha <nilishakhadgi29@gmail.com>
2024-12-28 15:46:48 +0545

    NavigationActivity

--- app/src/main/java/com/example/wt/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/example/wt/ui/activity/SplashActivity.kt
@@ -18,7 +18,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            val intent = Intent(this@SplashActivity, NavigationActivity::class.java)
             startActivity(intent)
             finish()
         }, 3000)
