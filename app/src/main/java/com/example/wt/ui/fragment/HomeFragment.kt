2b0452d9a0ce70101fffb0034f53193afd9f2ab8
nilisha <nilishakhadgi29@gmail.com>
2025-01-02 10:01:25 +0545

    HomeFragment

--- app/src/main/java/com/example/wt/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/wt/ui/fragment/HomeFragment.kt
@@ -1,10 +1,12 @@
 package com.example.wt.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import com.example.wt.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,12 +20,14 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -38,7 +42,7 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    companion object {
+        companion object {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
