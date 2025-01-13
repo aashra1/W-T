e27c9b2346f50cca7ecedd79e14453915ebcb08a
oshikadahal <149761380+oshikadahal@users.noreply.github.com>
2025-01-13 10:49:27 +0545

    Gradle changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,16 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.google.gms.google.services)
+
 }
 
 android {
+
+    buildFeatures {
+        viewBinding=true
+    }
     namespace = "com.example.wt"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.wt"
@@ -43,7 +48,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
