0825db588978a6ec935e521b5587505b08152597
Rikchhen <rikchhenlama3@gmail.com>
2025-01-21 12:16:36 +0545

    Login Backend

--- app/src/main/java/com/example/wt/repository/UserRepository.kt
+++ app/src/main/java/com/example/wt/repository/UserRepository.kt
@@ -4,6 +4,7 @@ import com.example.wt.model.UserModel
 import com.google.firebase.auth.FirebaseUser
 
 interface UserRepository {
+    fun login(email : String, password : String, callback:(Boolean,String) -> Unit)
     fun signup(email:String,password:String,callback:(Boolean,String,String)->Unit)
     fun getCurrentUser(): FirebaseUser?
     fun addUserToDB(userId:String,userModel:UserModel,callback: (Boolean, String) -> Unit)
