6026ad8f137c0ddf44ebb15a0ac1f89570fd3a49
Rikchhen <rikchhenlama3@gmail.com>
2025-01-28 14:12:49 +0545

    Product Backend

--- app/src/main/java/com/example/wt/model/ProductModel.kt
+++ app/src/main/java/com/example/wt/model/ProductModel.kt
@@ -1,4 +1,44 @@
 package com.example.wt.model
 
-class ProductModel {
+import android.icu.text.DecimalFormat
+import android.media.Image
+import android.os.Parcel
+import android.os.Parcelable
+
+data class ProductModel(
+    var productId : String = "",
+    var productImage : String = "",
+    var productName : String = "",
+    var productDesc : String = "",
+    var price : Int = 0
+) : Parcelable  {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()?:"",
+        parcel.readString()?:"",
+        parcel.readString()?:"",
+        parcel.readString()?:"",
+        parcel.readInt()?:0,
+    )
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(productId)
+        parcel.writeString(productImage)
+        parcel.writeString(productName)
+        parcel.writeString(productDesc)
+        parcel.writeInt(price)
+    }
+
+    companion object CREATOR : Parcelable.Creator<ProductModel> {
+        override fun createFromParcel(parcel: Parcel): ProductModel {
+            return ProductModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ProductModel?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
---
+++ app/src/main/java/com/example/wt/repository/ProductRepositoryImpl.kt
@@ -0,0 +1,98 @@
+package com.example.wt.repository
+
+import com.example.wt.model.ProductModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class ProductRepositoryImpl : ProductRepository {
+
+    val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+    val ref : DatabaseReference = database.reference.child("products")
+
+    override fun addProduct(
+        productModel: ProductModel,
+        callback: (Boolean, String) -> Unit
+    ) {
+        var id = ref.push().key.toString()
+        productModel.productId = id
+
+        ref.child(id).setValue(productModel).addOnCompleteListener{
+            if(it.isSuccessful){
+                callback(true,"Product Added Successfully")
+            } else{
+                callback(false,"${it.exception?.message}")
+            }
+        }
+    }
+
+    override fun updateProduct(
+        productId: String,
+        data: MutableMap<String, Any>,
+        callback: (Boolean, String) -> Unit
+    ) {
+        ref.child(productId).updateChildren(data).addOnCompleteListener{
+            if(it.isSuccessful){
+                callback(true,"Product Updated Successfully")
+            } else{
+                callback(false,"${it.exception?.message}")
+            }
+        }
+    }
+
+    override fun deleteProduct(
+        productId: String,
+        callback: (Boolean, String) -> Unit
+    ) {
+        ref.child(productId).removeValue().addOnCompleteListener{
+            if(it.isSuccessful){
+                callback(true,"Product Deleted Successfully")
+            } else{
+                callback(false,"${it.exception?.message}")
+            }
+        }
+    }
+
+    override fun getProductById(
+        productId: String,
+        callback: (ProductModel?, Boolean, String) -> Unit
+    ) {
+        ref.child(productId).addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    var model = snapshot.getValue(ProductModel::class.java)
+                    callback(model,true,"Product Fetched Successfully")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                callback(null,false,error.toString())
+            }
+
+        })
+    }
+
+    override fun getAllProduct(callback: (List<ProductModel>?, Boolean, String) -> Unit) {
+        ref.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var products = mutableListOf<ProductModel>()
+                if(snapshot.exists()){
+                    for(eachProduct in snapshot.children){
+                        var data = eachProduct.getValue(ProductModel::class.java)
+                        if(data != null){
+                            products.add(data)
+                        }
+                    }
+                    callback(products,true,"Product Added Successfully")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                callback(null,false,error.toString())
+            }
+
+        })
+    }
+}
