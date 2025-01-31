15e73d37440730de325d8e87c69d44b9adcccce9
oshikadahal <149761380+oshikadahal@users.noreply.github.com>
2025-01-31 10:54:06 +0545

    CartModel updated

--- app/src/main/java/com/example/wt/model/CartModel.kt
+++ app/src/main/java/com/example/wt/model/CartModel.kt
@@ -4,6 +4,11 @@ package com.example.wt.model
 data class CartModel(
     var cartId : String = "",
     var userId : String = "",
-    var productId: String = ""
+    var productId: String = "",
+    var addToCart :String = "",
+    var deleteCart : String = "",
+    var updateCart : String = "",
+    var getallcart : String = "",
+    var getcartId : String = "",
 ) {
 }
