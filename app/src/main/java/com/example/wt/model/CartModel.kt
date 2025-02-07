9c48b3899947791c5fa3a3addcab5aff9a62e860
oshikadahal <149761380+oshikadahal@users.noreply.github.com>
2025-02-07 10:35:38 +0545

    CartModel and CartRepositoryImp  updated

--- app/src/main/java/com/example/wt/model/CartModel.kt
+++ app/src/main/java/com/example/wt/model/CartModel.kt
@@ -1,13 +1,58 @@
 package com.example.wt.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 
 data class CartModel(
 
     var cartId : String = "",
     var userId : String = "",
     var productId: String = "",
+    var productImage : String = "",
+    var brandName : String = "",
+    var productName: String = "",
+    var price : Int = 0,
+    var size : String = "",
+    var quantity : Int = 0
 
+) : Parcelable{
+    constructor(parcel: Parcel) : this(
+        parcel.readString()?:"",
+        parcel.readString()?:"",
+        parcel.readString()?:"",
+        parcel.readString()?:"",
+        parcel.readString()?:"",
+        parcel.readString()?:"",
+        parcel.readInt()?:0,
+        parcel.readString()?:"",
+        parcel.readInt()?:0
+    ) {
+    }
 
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(cartId)
+        parcel.writeString(userId)
+        parcel.writeString(productId)
+        parcel.writeString(productImage)
+        parcel.writeString(brandName)
+        parcel.writeString(productName)
+        parcel.writeInt(price)
+        parcel.writeString(size)
+        parcel.writeInt(quantity)
+    }
 
-) {
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<CartModel> {
+        override fun createFromParcel(parcel: Parcel): CartModel {
+            return CartModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<CartModel?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
