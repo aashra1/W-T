3ddd399721c430aac760be393074db81dfb22e67
nilisha <nilishakhadgi29@gmail.com>
2025-02-10 11:05:10 +0545

    AddProductActivity

--- app/src/main/java/com/example/wt/ui/activity/AddProductActivity.kt
+++ app/src/main/java/com/example/wt/ui/activity/AddProductActivity.kt
@@ -1,21 +1,110 @@
 package com.example.wt.ui.activity
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.wt.R
+import com.example.wt.databinding.ActivityAddProductBinding
+import com.example.wt.model.ProductModel
+import com.example.wt.repository.ProductRepositoryImpl
+import com.example.wt.utils.ImageUtils
+import com.example.wt.utils.LoadingUtils
+import com.example.wt.viewModel.ProductViewModel
+import com.squareup.picasso.Picasso
 
 class AddProductActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityAddProductBinding
+
+    lateinit var productViewModel: ProductViewModel
+
+    lateinit var loadingUtils: LoadingUtils
+
+    lateinit var imageUtils: ImageUtils
+
+    var imageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_product)
+        binding = ActivityAddProductBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loadingUtils = LoadingUtils(this)
+
+        imageUtils = ImageUtils(this)
+
+        var repo = ProductRepositoryImpl()
+        productViewModel = ProductViewModel(repo)
+
+
+        imageUtils.registerActivity { url ->
+            url.let { it ->
+                imageUri = it
+                Picasso.get().load(it).into(binding.productImage)
+            }
+        }
+        binding.productImage.setOnClickListener {
+            imageUtils.launchGallery(this)
+        }
+        binding.addProductButton.setOnClickListener {
+            uploadImage()
+
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun uploadImage() {
+        loadingUtils.show()
+        imageUri?.let { uri ->
+            productViewModel.uploadImage(this, uri) { imageUrl ->
+                Log.d("checpoirs", imageUrl.toString())
+                if (imageUrl != null) {
+                    addProduct(imageUrl)
+                } else {
+                    Log.e("Upload Error", "Failed to upload image to Cloudinary")
+                }
+            }
+        }
+    }
+
+    private fun addProduct(imageUrl: String) {
+        var brandName = binding.brandName.text.toString()
+        var productName = binding.productName.text.toString()
+        var productPrice = binding.price.text.toString().toInt()
+
+        var model = ProductModel(
+            "",
+            imageUrl,
+            brandName,
+            productName, productPrice,
+        )
+
+        productViewModel.addProduct(model) { success, message ->
+            if (success) {
+                Toast.makeText(
+                    this@AddProductActivity,
+                    message, Toast.LENGTH_LONG
+                ).show()
+                finish()
+                loadingUtils.dismiss()
+            } else {
+                Toast.makeText(
+                    this@AddProductActivity,
+                    message, Toast.LENGTH_LONG
+                ).show()
+                loadingUtils.dismiss()
+            }
         }
     }
 }
