c6e245d61c7b358186d4930a491057dfa03b3034
Rikchhen <rikchhenlama3@gmail.com>
2025-02-10 10:37:32 +0545

    Updated ViewModel & Added Loading

--- app/src/main/java/com/example/wt/utils/LoadingUtils.kt
+++ app/src/main/java/com/example/wt/utils/LoadingUtils.kt
@@ -1,4 +1,27 @@
 package com.example.wt.utils
 
-class LoadingUtils {
+import android.app.Activity
+import android.app.AlertDialog
+import com.example.wt.R
+
+class LoadingUtils(val activity : Activity) {
+
+    lateinit var alertDialog: AlertDialog
+
+    fun show(){
+        val builder = AlertDialog.Builder(activity)
+
+        val designView = activity.layoutInflater.inflate(R.layout.loading,null)
+
+        builder.setView(designView)
+        builder.setCancelable(false)
+
+        alertDialog = builder.create()
+        alertDialog.show()
+    }
+
+    fun dismiss(){
+        alertDialog.dismiss()
+    }
+
 }
