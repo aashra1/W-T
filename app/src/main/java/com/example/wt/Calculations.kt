1be3a9dd189a8e362737e57191061533b3f3b7c7
oshikadahal <149761380+oshikadahal@users.noreply.github.com>
2025-03-05 20:23:59 +0545

    Instrumented Testing

---
+++ app/src/main/java/com/example/wt/Calculations.kt
@@ -0,0 +1,4 @@
+package com.example.wt
+
+class Calculations {
+}
---
+++ app/src/main/java/com/example/wt/ui/activity/CalculationsActivity.kt
@@ -0,0 +1,35 @@
+package com.example.wt.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.wt.Calculations
+import com.example.wt.R
+import com.example.wt.databinding.ActivityCalculationsBinding
+
+class CalculationsActivity : AppCompatActivity() {
+    lateinit var binding : ActivityCalculationsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityCalculationsBinding .inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttoncalculate.setOnClickListener {
+            var calculations = Calculations()
+            var a = binding.firstvalue.text.toString().toInt()
+            var b = binding.secondvalue.text.toString().toInt()
+            var result = calculations.add(a,b)
+
+            binding.displaySum.text = result.toString()
+
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
